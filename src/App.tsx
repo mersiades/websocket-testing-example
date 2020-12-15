41bb930d8ccb1d0c84d3a3d1a3b8a4029aa84d43
Michael Mersiades <fakename014@gmail.com>
2020-12-15 15:32:56 +1100

    Adds SockJS fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,46 @@
-import React, { FC, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { WebsocketStore } from './@types/storeInterfaces';
-import { connectWebstompClient } from './services/webstompService';
+import React, { FC, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { WebsocketStore } from "./@types/storeInterfaces";
+import { connectWebstompClient } from "./services/webstompService";
 
 const App: FC = () => {
-  const stompClient = useSelector(({ stompClient }: WebsocketStore) => stompClient);
-  const messages = useSelector(({messages}: WebsocketStore) => messages);
+  const stompClient = useSelector(
+    ({ stompClient }: WebsocketStore) => stompClient
+  );
+  const messages = useSelector(({ messages }: WebsocketStore) => messages);
 
   useEffect(() => {
-    !stompClient && connectWebstompClient()
-  }, [stompClient])
+    !stompClient && connectWebstompClient();
+  }, [stompClient]);
 
   const sendMessage = () => {
-    console.log('clicked')
-    stompClient?.send('/app/test', JSON.stringify({ chatId: "100000", message: "message", sender: "me"}))
-  }
+    console.log("clicked");
+    stompClient?.send(
+      "/app/test",
+      JSON.stringify({ chatId: "100000", message: "message", sender: "me" })
+    );
+  };
 
-  console.log('stompClient', stompClient)
   if (!!stompClient && stompClient.connected) {
     return (
-    <>
-      <p>Websocket: Connected</p>
-      <div>
-        <button disabled={!stompClient} onClick={() => sendMessage()}>ADD MESSAGE</button>
-      </div>
-      <div>
-        <h3>Number of messages: <span data-test="message-count">{messages.length}</span></h3>
-      </div>
-    </>
-    )
+      <>
+        <p>Websocket: Connected</p>
+        <div>
+          <button disabled={!stompClient} onClick={() => sendMessage()}>
+            ADD MESSAGE
+          </button>
+        </div>
+        <div>
+          <h3>
+            Number of messages:{" "}
+            <span data-test="message-count">{messages.length}</span>
+          </h3>
+        </div>
+      </>
+    );
   } else {
-    return <p>Websocket: Not connected</p>
+    return <p>Websocket: Not connected</p>;
   }
-}
+};
 
 export default App;
--- src/services/webstompService.ts
+++ src/services/webstompService.ts
@@ -1,20 +1,33 @@
-import webstomp, { Message } from 'webstomp-client';
+import SockJS from 'sockjs-client';
+import webstomp, { Client, Frame, Message } from 'webstomp-client';
 import { addClient, addMessage } from '../actions/websocketActions'
 import store from '../store';
 
 export const connectWebstompClient = () => {
   console.log('connecting webstomp client')
-  const stompClient = webstomp.client('ws://localhost:8081/chat')
+  const websocketsAreNotAvailableInTheBrowser = typeof WebSocket !== 'function';
+  let stompClient: Client
+  if (websocketsAreNotAvailableInTheBrowser) {
+    stompClient = webstomp.over(new SockJS('http://localhost:8081/test'))
+  } else {
+    stompClient = webstomp.client('ws://localhost:8081/test', {
+      // heartbeat: { incoming: 10000, outgoing: 10000},
+      heartbeat: false,
+      protocols: webstomp.VERSIONS.supportedProtocols()
+    })
+  }
   
-  stompClient.connect({/* no headers */}, () => {
-    console.log('subscribing to /topic/test')
-     stompClient.subscribe('/topic/test', (payload: Message) => showTextMessage(payload))
-     
-     // Puts stomp client in the Redux store
-     store.dispatch<any>(addClient(stompClient));
-  })
+  stompClient.connect({/* no headers */}, () => onConnect(), (error: Frame | CloseEvent) => onError(error))
 
-  
+  const onConnect = () => {
+    console.log('subscribing to /topic/test')
+    stompClient.subscribe('/topic/test', (payload: Message) => showTextMessage(payload))
+     
+    // Puts stomp client in the Redux store
+    store.dispatch<any>(addClient(stompClient));
+  }
+
+  const onError = (error: Frame | CloseEvent) => console.error(error)
 
   return stompClient
 }
